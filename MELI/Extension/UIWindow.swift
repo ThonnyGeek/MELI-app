a74d554bcb0fa6f6649e6a47c9007648c0f26d12
Anthony <thonnygonz@gmail.com>
2024-03-01 18:58:20 -0500

    Adding funct to get screen size

--- MELI/Extension/UIWindow.swift
+++ MELI/Extension/UIWindow.swift
@@ -5,4 +5,23 @@
 //  Created by Thony Gonzalez on 1/03/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension UIWindow {
+    static var current: UIWindow? {
+        for scene in UIApplication.shared.connectedScenes {
+            guard let windowScene = scene as? UIWindowScene else { continue }
+            for window in windowScene.windows {
+                if window.isKeyWindow { return window }
+            }
+        }
+        return nil
+    }
+}
+
+
+extension UIScreen {
+    static var current: UIScreen? {
+        UIWindow.current?.screen
+    }
+}
