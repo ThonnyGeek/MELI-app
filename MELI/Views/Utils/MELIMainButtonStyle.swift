a079b2049842934324dfb44fcfcbd64c0c0544df
Anthony <thonnygonz@gmail.com>
2024-03-01 18:58:53 -0500

    Adding main button style

--- MELI/Views/Utils/MELIMainButtonStyle.swift
+++ MELI/Views/Utils/MELIMainButtonStyle.swift
@@ -7,12 +7,43 @@
 
 import SwiftUI
 
-struct MELIMainButtonStyle: View {
+public struct MELIMainButtonStyle: ButtonStyle {
+    public func makeBody(configuration: Configuration) -> some View {
+        MELIMainButtonStyleView(configuration: configuration)
+    }
+}
+
+private extension MELIMainButtonStyle {
+    struct MELIMainButtonStyleView: View {
+        let configuration: MELIMainButtonStyle.Configuration
+
+        var body: some View {
+            return configuration.label
+                .font(.manropeBold(16))
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 15)
+                .background {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(.mainBlue)
+                }
+                .padding(10)
+                .opacity(configuration.isPressed ? 0.8 : 1.0)
+                .scaleEffect(configuration.isPressed ? 0.98 : 1.0)
+        }
+    }
+}
+
+
+struct MELIMainButtonStylePreview: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Main button") {
+            //
+        }
+        .buttonStyle(MELIMainButtonStyle())
     }
 }
 
 #Preview {
-    MELIMainButtonStyle()
+    MELIMainButtonStylePreview()
 }
